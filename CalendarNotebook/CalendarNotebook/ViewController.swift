2145e0164ac621219fdb9f0688914064c954058c
Katherine Li <katherineli0514@gmail.com>
2018-10-21 18:37:17 -0700

    Add initial project.

---
+++ CalendarNotebook/CalendarNotebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarNotebook
+//
+//  Created by Katherine Li on 10/21/18.
+//  Copyright © 2018 Katherine Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
